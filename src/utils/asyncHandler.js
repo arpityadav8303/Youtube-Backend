07be3204719da0399df3a6147569f3e1c9969a23
Arpit <arpityadav58571@gmail.com>
2025-10-25 22:04:26 +0530

    jwtbcrypt

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,25 @@
-const asyncHandler=(fn)=>async(req,res,next)=>{
-   try{
-      await fn(req,res,next)
-   }
-   catch(error){
-      res.status(error.code||500).json({
-         success:false,
-         message:error.message
-      })
+const asyncHandler=(requestHandler)=>{(req,res,next)=>{
+    promise.resolve(requestHandler(req,res,next)).catch(err=>{
+        next(err)
+    })
    }
 }
+
+export {asyncHandler}
+
+
+
+
+
+
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+//    try{
+//       await fn(req,res,next)
+//    }
+//    catch(error){
+//       res.status(error.code||500).json({
+//          success:false,
+//          message:error.message
+//       })
+//    }
+// }
